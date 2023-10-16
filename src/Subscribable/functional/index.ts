3e83e30a3b5f5d3ab42991defcbc73e22f46f016
Kunal Sethi <ksethi2211@gmail.com>
2023-10-16 17:09:29 -0400

    Implement pubsub as a functional method

---
+++ src/Subscribable/functional/index.ts
@@ -0,0 +1,18 @@
+const createSubscribable = <MessageType>() => {
+  const subscribers: Set<(msg: MessageType) => void> = new Set();
+
+  return {
+    subscribe(cb: (msg: MessageType) => void): () => void {
+      subscribers.add(cb);
+      return () => {
+        subscribers.delete(cb);
+      };
+    },
+
+    publish(msg: MessageType): void {
+      subscribers.forEach((cb) => cb(msg));
+    },
+  };
+};
+
+export default createSubscribable;
